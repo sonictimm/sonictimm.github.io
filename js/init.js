af116300e283b841d897147e4d83220fd990dffb
Timmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm <sonictimm@gmail.com>
2020-05-08 23:40:41 -0400

    Update description to reflect my current position at Virtual Heroes
    Also included some fun in the changing text at the top

--- js/init.js
+++ js/init.js
@@ -452,7 +452,7 @@ function edina_tm_animate_text(){
 	var animateSpan			= jQuery('.edina_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["GAME DEVELOPER", "PROGRAMMER", "LEVEL DESIGNER", "HACKER"],
+			strings: ["UNREAL DEVELOPER", "C++ PROGRAMMER", "VR GUY", "GOOD ENOUGH AT UI", "WHY ARE YOU STILL READING THIS?", "¯\\_(ツ)_/¯"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
